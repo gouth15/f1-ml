9636935463bda916d8fbf1b8e95ae5b89dc6baa4
gouth15 <gouthams.330@gmail.com>
2025-08-16 22:26:45 +0530

    Training - 1.0

---
+++ train.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import root_mean_squared_error
+from pathlib import Path
+
+# LOAD THE DATA
+CSV_PATH = Path("new_data.csv")
+data = pd.read_csv(CSV_PATH)
+
+# SET THE FEATURE AND TARGET COLUMN
+TARGET_COLUMN = "LapTime"
+FEATURE_COLUMNS = data.columns.to_list()
+FEATURE_COLUMNS.remove(TARGET_COLUMN)
+
+X = data[FEATURE_COLUMNS]
+Y = data[TARGET_COLUMN]
+
+# SPLIT THE TRAIN AND TEST DATA SET
+XTrain, XTest, YTrain, YTest = train_test_split(X, Y, test_size=0.3)
+
+# DEFINE THE MODEL
+model = DecisionTreeRegressor()
+
+# FIT THE MODEL
+model.fit(XTrain, YTrain)
+
+# PREDICT AND EVALUATE THE ERROR
+predictions = model.predict(XTest)
+rmse = root_mean_squared_error(YTest, predictions)
+
+# SAVE THE MODEL
+with open("decisiontree_v1.pkl", "wb") as file:
+    pickle.dump(model, file)
+
