cddf05a513956dae57792017b4ca5ec567f4870c
gouth15 <gouthams.330@gmail.com>
2025-08-16 22:01:34 +0530

    Preprocessing - 1.0

--- preprocess.py
+++ preprocess.py
@@ -1,59 +1,36 @@
-from pathlib import Path
 import pandas as pd
-import json
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler
-import joblib
+import numpy as np
+from pathlib import Path
 
-class F1DataPreprocessor:
-    def __init__(self, csv_path="new.csv", rows_json="row_to_include.json", compound_json="compound_map.json"):
-        self.csv_path = Path(csv_path)
-        with open(rows_json, "r") as f:
-            self.rows_to_include = json.load(f)
-        with open(compound_json, "r") as f:
-            self.compound_map = json.load(f)
-        self.encoders = {}
-        self.scaler = None
+# LOAD THE DATA CSV
+CSV_PATH = Path("data.csv")
+data = pd.read_csv(CSV_PATH)
 
-    def convert_tyre_compound(self, compound: str):
-        return self.compound_map.get(compound, None)
+# REMOVE FP1 ROOKIE SESSION
+DRIVERS = [1, 4, 5, 6, 10, 12, 14, 16, 18, 22, 23, 27, 30, 31, 44, 55, 63, 81, 87, 43]
+data = data[data["DriverNumber"].isin(DRIVERS)]
 
-    def convert_timestamp(self, timestamp):
-        return pd.to_timedelta(timestamp).total_seconds()
+# FILTER THE COLUMNS THAT USED FOR TRAINING
+INCLUDE= [
+    "DriverNumber", "LapTime", "SpeedI1", "SpeedI2", "SpeedST",
+    "SpeedFL", "Compound", "TyreLife", "FreshTyre", "TrackStatus",
+    "TrackLength"]
+data = data[INCLUDE]
 
-    def encode_column(self, data, column):
-        le = LabelEncoder()
-        data[column] = le.fit_transform(data[column])
-        self.encoders[column] = le
-        return data
+# REMOVE NA ROWS
+data = data.dropna()
 
-    def save_encoders(self, path="encoders.pkl"):
-        joblib.dump(self.encoders, path)
+# CONVERT THE DTYPES 
+COMPOUND_MAPPING = { 
+    "SOFT": 1, "MEDIUM": 2, "HARD": 3,
+    "INTERMEDIATE": 4, "WET": 5 
+}
+data["LapTime"] = data["LapTime"].apply(lambda x: pd.to_timedelta(x).total_seconds())
+data["Compound"] = data["Compound"].apply(lambda x: COMPOUND_MAPPING[x])
+data["FreshTyre"] = data["FreshTyre"].apply(lambda x: 1 if x else 0)
 
-    def normalize_columns(self, data, exclude_columns=None):
-        """Normalize numeric columns except those in exclude_columns."""
-        if exclude_columns is None:
-            exclude_columns = []
+# ADD SOME USEFUL DATA
+data["AveragePace"] = data["TrackLength"] / (data["LapTime"] / 3600 )
 
-        numeric_cols = data.select_dtypes(include=['int64', 'float64']).columns
-        cols_to_scale = [col for col in numeric_cols if col not in exclude_columns]
-
-        self.scaler = MinMaxScaler()
-        data[cols_to_scale] = self.scaler.fit_transform(data[cols_to_scale])
-        return data
-
-    def preprocess(self, save_encoders_path="encoders.pkl", normalize=True, exclude_normalize=None):
-        data = pd.read_csv(self.csv_path, usecols=self.rows_to_include).dropna()
-        data["Compound"] = data["Compound"].apply(self.convert_tyre_compound)
-
-        for column in ["LapTime", "Sector1Time", "Sector2Time", "Sector3Time"]:
-            data[column] = data[column].apply(self.convert_timestamp)
-
-        data["SpeedSTD"] = data[["SpeedI1", "SpeedI2", "SpeedFL", "SpeedST"]].std(axis=1)
-        data["SpeedMean"] = data[["SpeedI1", "SpeedI2", "SpeedFL", "SpeedST"]].mean(axis=1)
-        data["AveragePage"] = data["TrackLength"] / (data["LapTime"] / 3600)
-
-        if normalize:
-            data = self.normalize_columns(data, exclude_columns=exclude_normalize)
-
-        data.to_csv(self.csv_path, index=False)
-        self.save_encoders(save_encoders_path)
+# SAVE THE PREPROCESSED DATA
+data.to_csv("new_data.csv", index=False)
